a85e4c21bd5b8253e2cc54600179b502a85a644d
Christian López García <68792480+drownedd@users.noreply.github.com>
2023-03-24 09:31:16 +0100

    initial models

--- project/web/admin.py
+++ project/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from web.models import *
 
 # Register your models here.
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Language)
+admin.site.register(Genre)
+admin.site.register(Film)
