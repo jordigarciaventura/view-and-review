24057738c25f25af7f410722896c61a63642dae7
jordigarciaventura <jordigarciaventura@gmail.com>
2023-04-04 21:14:37 +0000

    Refined comment

--- django/project/settings/base.py
+++ django/project/settings/base.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('SECRET_KEY', "foo")
