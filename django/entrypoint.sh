b549009e50e9034023dfa11c8e219ce0036ad6dd
jordigarciaventura <jordigarciaventura@gmail.com>
2023-04-06 16:56:56 +0200

    Simplified entrypoint

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Run any database migrations
-if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "False" ]; then
-  echo "Running database migrations..."
-  python manage.py migrate --noinput
-fi
+# if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "False" ]; then
+#   echo "Running database migrations..."
+#   python manage.py migrate --noinput
+# fi
 
 exec "$@"
