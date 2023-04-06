e8781bdf84cf0d3f3e3065a52915b75c8eb04da4
Christian López García <68792480+drownedd@users.noreply.github.com>
2023-04-06 19:01:36 +0200

    impl register

--- django/web/urls.py
+++ django/web/urls.py
@@ -4,10 +4,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('film/<pk>', views.FilmView.as_view(), name='film'),
     path('logout/', views.LogoutView.as_view(), name='logout-page'),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', views.RegisterView, name='register'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
