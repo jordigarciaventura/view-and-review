fb43e85a5f5e8ba2dd61bd62260b229201714db7
drownedd <drwn.it@gmail.com>
2023-04-29 19:25:05 +0200

    bootstrapped registration

--- django/web/forms.py
+++ django/web/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -5,10 +6,19 @@ from django.contrib.auth.models import User
 import web.models
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'E-mail'}))
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+    
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Repeat password'
     
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
