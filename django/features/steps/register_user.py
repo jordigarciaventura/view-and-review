b3f1355abdbebe3acd3a6bddb80466131733d6c2
drownedd <drwn.it@gmail.com>
2023-05-17 17:37:57 +0200

    register_user feature done

---
+++ django/features/steps/register_user.py
@@ -0,0 +1,21 @@
+from django.db.models import Q
+from behave import *
+
+from web.models import User
+
+use_step_matcher("parse")
+
+@when('I register a new user')
+def step_impl(context):
+    for row in context.table:
+        context.browser.visit(context.get_url('register'))
+        if context.browser.url == context.get_url('register'):
+            form = context.browser.find_by_id('register-form')
+            for heading in row.headings:
+                context.browser.fill(heading, row[heading])
+            form.find_by_value('register').click()
+    assert User.objects.count() == 1
+        
+@then(u'I\'m viewing the main page')
+def step_impl(context):
+    assert context.get_url('index') in context.browser.url # Check if it is substring due to deep-linking
--- django/project/settings/base.py
+++ django/project/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'web',
     'bootstrap5',
     'compressor',
+    'behave_django',
 ]
 
 MIDDLEWARE = [
