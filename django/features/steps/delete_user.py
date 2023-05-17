7d1db7270879e24d800fb439eb4812d31b93f45d
drownedd <drwn.it@gmail.com>
2023-05-17 18:08:30 +0200

    assert instead of if's

--- django/features/steps/delete_user.py
+++ django/features/steps/delete_user.py
@@ -9,10 +9,10 @@ use_step_matcher("parse")
 def step_impl(context):
     assert User.objects.count() == 1
     context.browser.visit(context.get_url('user-settings'))
-    if context.browser.url == context.get_url('user-settings'):
-        context.browser.find_by_id('delete-button').click()
-        if context.browser.url == context.get_url('user-delete'):
-            context.browser.find_by_value('Confirm').first.click()
+    assert context.browser.url == context.get_url('user-settings')
+    context.browser.find_by_id('delete-button').click()
+    assert context.browser.url == context.get_url('user-delete')
+    context.browser.find_by_value('Confirm').first.click()
     
     
 @then(u'There are no users')
--- django/features/steps/register_user.py
+++ django/features/steps/register_user.py
@@ -9,11 +9,11 @@ use_step_matcher("parse")
 def step_impl(context):
     for row in context.table:
         context.browser.visit(context.get_url('register'))
-        if context.browser.url == context.get_url('register'):
-            form = context.browser.find_by_id('register-form')
-            for heading in row.headings:
-                context.browser.fill(heading, row[heading])
-            form.find_by_value('register').click()
+        assert context.browser.url == context.get_url('register'):
+        form = context.browser.find_by_id('register-form')
+        for heading in row.headings:
+            context.browser.fill(heading, row[heading])
+        form.find_by_value('register').click()
     assert User.objects.count() == 1
         
 @then(u'I\'m viewing the main page')
