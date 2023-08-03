9b21647f026fa119a7d463b8c1cb7c557ac8b7bd
Jordi García Ventura <jordigarciaventura@gmail.com>
2023-08-03 18:58:33 +0200

    Fix load environment file using dotenv

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Load environment variables from file
-source .env
-
 if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "False" ]; then
   echo "Running database migrations..."
   python manage.py migrate --noinput
