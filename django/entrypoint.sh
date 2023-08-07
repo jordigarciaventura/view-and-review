3d45c627e7ffd414bc2d29fcdbc3429c80cd3197
Jordi García Ventura <jordigarciaventura@gmail.com>
2023-08-08 01:16:27 +0200

    Fix missing environment variables during build

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-ENV_PATH=/etc/secrets/.env
-
-# Load environment variables
-if [[ -f  $ENV_PATH ]] ; then
-  echo "Loading environment variables..."
-else
-  echo "File $ENV_PATH not found, aborting."
-  exit 1
-fi
-
-while read -r LINE; do
-  if [[ $LINE == *'='* ]] && [[ $LINE != '#'* ]]; then
-    ENV_VAR="$(echo $LINE | envsubst)"
-    eval "declare $ENV_VAR"
-  fi
-done < $ENV_PATH
-
 # Apply database migrations
-if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "False" ]; then
+if [ "$DJANGO_MIGRATE_DB_ON_STARTUP" != "false" ]; then
   echo "Running database migrations..."
   python manage.py migrate --noinput
 fi
